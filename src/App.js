edc0ef75f572d6a9dfbf25df44774dd7f6747112
Dvir <dvirbe@gmail.com>
2024-02-05 00:31:18 +0200

    .

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import colorPicker from "./Components/ColorPicker";
 
 class App extends React.Component {
     state = {
-        gameInProgress :false,
+        gameInProgress: false,
         isPlayerOneTurn: true,
         player1_Color: "#f60000",
         player2_Color: "#e3ff00",
         someoneWin: false,
+        rows: 6,
+        columns: 7,
         winner: 0,
 
     }
@@ -25,7 +27,6 @@ class App extends React.Component {
     }
 
 
-
     setColor = (newColor) => {
         this.setState({player1_Color: newColor})
     }
@@ -33,31 +34,58 @@ class App extends React.Component {
         this.setState({player2_Color: newColor})
     }
 
+    handleRowInput = (event) => {
+        this.setState({rows: event.target.value})
+    }
+    handleColumnInput = (event) => {
+        this.setState({columns: event.target.value})
+    }
 
     render() {
         return (<>
-                <header className={"header"}>
-                    <div> connect four game</div>
-                </header>
-                {this.state.gameInProgress ? (
-                    <div>
-                        <Board isPlayerOneTurn={this.state.isPlayerOneTurn}
-                               changePlayerTurn={()=>{this.changeTurn()}}
-                               color={[this.state.player1_Color, this.state.player2_Color] }/>
-                    </div>
-                ) : (
-                    <div className="App">
-                        <ColorPicker setColor={this.setColor} player={1}/>
-                        <ColorPicker setColor={this.setColor2} player={2}/>
-                        {<button onClick={() => {
-                            this.startGame()
-                        }}>start game</button>}
+                <div className={"App"}>
+                    <header>
+                        <div> connect four</div>
+                    </header>
 
-                        <div className="color-picker-container">
-                            <input type="number" value={6} />
+
+                    {this.state.gameInProgress ? (
+
+
+                        <div>
+
+                            <Board isPlayerOneTurn={this.state.isPlayerOneTurn}
+                                   changePlayerTurn={() => {
+                                       this.changeTurn()
+                                   }}
+                                   rows={this.state.rows}
+                                   columns={this.state.columns}
+                                   color={[this.state.player1_Color, this.state.player2_Color]}/>
                         </div>
-                    </div>
-                )}
+                    ) : (
+                        <div>
+                            <ColorPicker setColor={this.setColor} player={1}/>
+                            <ColorPicker setColor={this.setColor2} player={2}/>
+
+                            <div className="input">
+                                <input type="number"
+                                       value={this.state.rows}
+                                       min={1}
+                                       onInputCapture={this.handleRowInput}/>
+                            </div>
+                            <div className="input">
+                                <input type="number"
+                                       value={this.state.columns}
+                                       onInputCapture={this.handleColumnInput}/>
+                            </div>
+
+                            {<button onClick={() => {
+                                this.startGame()
+                            }}>start game</button>}
+                        </div>
+                    )}
+                </div>
+
             </>
         )
     }
--- src/Components/Board.jsx
+++ src/Components/Board.jsx
@@ -3,16 +3,18 @@ import Disk from "./Disk";
 
 function Board(props) {
 
-    const numberOfRows = 6;
-    const numberOfColumns = 7;
+    const numberOfColumns = Number(props.columns);
+    const numberOfRows = Number(props.rows);
+    console.log(numberOfRows)
 
     const makeTable = () => {
         const temp = []
         for (let i = 0; i < numberOfColumns; i++) {
-
             const innerArray = new Array(numberOfRows).fill(0);
+
             temp.push(innerArray);
         }
+        console.log(numberOfRows)
         return temp
     }
     const [boardLayout, setBoardLayout] = useState(() => makeTable());
@@ -24,7 +26,6 @@ function Board(props) {
                 props.isPlayerOneTurn ? tempBoard[column - 1][i] = 1 :
                     tempBoard[column - 1][i] = 2;
                 setBoardLayout(tempBoard);
-                setTest(aa)
                 props.changePlayerTurn();
                 checkWin()
                 break;
@@ -34,24 +35,29 @@ function Board(props) {
 
     //
     function connectFourChecker(xOffset, yOffset) {
-        for (let x = 0; x < numberOfRows-1; x++) {
-            for (let y = 0; y < numberOfColumns-1; y++) {
-                if ( boardLayout[x][y]!==0) {
+        for (let x = 0; x < numberOfColumns ; x++) {
+            console.log(x)
+            for (let y = 0; y < numberOfRows ; y++) {
+                if (boardLayout[x][y] === 1 || boardLayout[x][y] === 2) {
                     console.log("******************")
-                    console.log(x+" "+y)
-                    console.log(boardLayout[x][y]+"start")
+                    console.log(x + " " + y)
+                    console.log(boardLayout[x][y] + "start")
+                    console.log(  x + (xOffset * 3) < numberOfRows &&
+                        x + (xOffset * 3) >= 0 &&
+                        y + (yOffset * 3) <= numberOfColumns &&
+                        y + (yOffset * 3) >= 0 )
                     if (
-                        x + (xOffset * 3) < numberOfRows &&
-                        x + (xOffset * 3 )>= 0 &&
-                        y + (yOffset * 3) < numberOfColumns &&
+                        x + (xOffset * 3) <= numberOfRows &&
+                        x + (xOffset * 3) >= 0 &&
+                        y + (yOffset * 3) <= numberOfColumns &&
                         y + (yOffset * 3) >= 0
                     ) {
                         if (boardLayout[x][y] === boardLayout[x + xOffset][y + yOffset]) {
-                            console.log("passed first "+[x + (xOffset)]+","+[y + (yOffset)])
+                            console.log("passed first " + [x + (xOffset)] + "," + [y + (yOffset)])
                             if (boardLayout[x][y] === boardLayout[x + (xOffset * 2)][y + (yOffset * 2)]) {
-                                console.log("passed second "+[x + (xOffset * 2)]+","+[y + (yOffset * 2)])
+                                console.log("passed second " + [x + (xOffset * 2)] + "," + [y + (yOffset * 2)])
                                 if (boardLayout[x][y] === boardLayout[x + (xOffset * 3)][y + (yOffset * 3)]) {
-                                    console.log("passed third "+[x + (xOffset * 3)]+","+[y + (yOffset * 3)])
+                                    console.log("passed third " + [x + (xOffset * 3)] + "," + [y + (yOffset * 3)])
                                     return boardLayout[x][y]
                                 }
                             }
@@ -64,49 +70,29 @@ function Board(props) {
     }
 
     function checkWin() {
-        let temp= connectFourChecker(1, 0)
-        if (temp  !== 0) {
-            console.log(temp+"winner")
-            return temp
-        }
-        temp= connectFourChecker(0, 1)
-        if (temp  !== 0) {
-            console.log(temp+"winner")
-            return temp
-        }
-        temp= connectFourChecker(1, 1)
-        if (temp  !== 0) {
-            console.log(temp+"winner")
-            return temp
-        }
-        temp =connectFourChecker(-1, 1)
+        let temp = connectFourChecker(1, 0)
         if (temp !== 0) {
-            console.log(temp+"winner")
+            console.log(temp + "winner")
+            return temp
+        }
+         temp = connectFourChecker(0, -1)
+        if (temp !== 0) {
+            console.log(temp + "winner")
+            return temp
+        }
+         temp = connectFourChecker(1, 1)
+        if (temp !== 0) {
+            console.log(temp + "winner")
+            return temp
+        }
+         temp = connectFourChecker(1, -1)
+        if (temp !== 0) {
+            console.log(temp + "winner")
             return temp
         }
         console.log("--------------------------")
         return 0;
     }
-
-    const aa = () => {
-        return (<>
-                {boardLayout.map((columns, columnIndex) =>
-                    <div>
-                        {columns.map((rows, rowIndex) =>
-                            <Disk color={props.color}
-                                  row={rowIndex + 1}
-                                  columns={columnIndex + 1}
-                                  clicker={handleDiskPlacementInColumn}
-                                  type={diskType(rows)}/>
-                        )}
-                    </div>
-                )}
-            </>
-
-        )
-    }
-    const [test, setTest] = useState(aa())
-
 
     const drawBoard = boardLayout.map((columns, columnIndex) =>
         <div>
